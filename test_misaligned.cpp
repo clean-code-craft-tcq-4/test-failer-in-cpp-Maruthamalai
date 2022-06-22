bc835a5b8c403c3de438120b5dd0f85f439fef0d
Maruthamalai <58974695+Maruthamalai@users.noreply.github.com>
2022-06-23 01:20:53 +0530

    Add files via upload

--- test_misaligned.cpp
+++ test_misaligned.cpp
@@ -6,9 +6,9 @@ int main() {
     int result = printColorMap();
 
     assert(result == 25);
-    assert(std::strcmp(printLine(0, 0), "1   |  White      |    Blue"));
-    assert(std::strcmp(printLine(2, 0), "10  |  Red        |    Slate"));
-    assert(std::strcmp(printLine(4, 0), "17  |  Yellow     |    Orange"));
+    assert(strcmp(printLine(0, 0), "1   |  White      |    Blue"));
+    assert(strcmp(printLine(2, 0), "10  |  Red        |    Slate"));
+    assert(strcmp(printLine(4, 0), "17  |  Yellow     |    Orange"));
 
     std::cout << "All is well (maybe!)\n";
     return 0;
