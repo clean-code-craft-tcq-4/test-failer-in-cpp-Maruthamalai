672e98ba99bb0a0aa45af029ac7c0a64774398fe
Maruthamalai <58974695+Maruthamalai@users.noreply.github.com>
2022-06-30 08:43:35 +0530

    Add files via upload

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,17 +1,18 @@
 #include <assert.h>
-
+#include <iomanip>
 #include "misaligned.hpp"
-
+using namespace std;
 
 std::string printLine(int majorColorIdx, int minorColorIdx)
 {
-    std::string str;
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-    str =  std::to_string(majorColorIdx * 5 + minorColorIdx ) + " | " + majorColor[majorColorIdx] + " | " + minorColor[majorColorIdx];
-    
-    return str;
+    //cout<<left<<setw(4)<<(majorColorIdx * 5 + minorColorIdx ) << " | " <<left<<setw(10)<< majorColor[majorColorIdx] << " | " <<left<<setw(10)<< minorColor[majorColorIdx];
+    std::ostringstream a;
+    a<<left<<setw(4)<<(majorColorIdx * 5 + minorColorIdx ) << " | " <<left<<setw(10)<< majorColor[majorColorIdx] << " | " <<left<<setw(10)<< minorColor[majorColorIdx]<<"\0";
+    cout<<a.str()<<endl;
+    return a.str();
 }
 
 int printColorMap() {
@@ -19,7 +20,7 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << printLine(i, j)<< "\n";
+            std::cout << printLine(i, j)<<endl;
         }
     }
     return i * j;
